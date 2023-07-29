7c040ae4de1012c98ddafbdd4b377045609d4a48
Xavier Mukodi <xaviermukodi@gmail.com>
2023-07-30 00:15:30 +0200

    added symlink in ui/src pointing to api/constants.js (experimental)

--- ui/src/constants.js
+++ ui/src/constants.js
@@ -1,4 +1,14 @@
 
+const {
+   CASE_SOURCES,
+   CASE_STATUS,
+   GENDER,
+   MARITAL_STATUS,
+   PROVINCES,
+   USER_ROLES,
+
+} = require('./backend-constants');
+
 
 const ACTION_TYPES = {
    SET_AUTHENTICATED: 'set-authenticated',
@@ -6,6 +16,13 @@ const ACTION_TYPES = {
    SET_USER: 'set-user',
 }
 
+
 export  {
    ACTION_TYPES,
+   CASE_SOURCES,
+   CASE_STATUS,
+   GENDER,
+   MARITAL_STATUS,
+   PROVINCES,
+   USER_ROLES,
 }
