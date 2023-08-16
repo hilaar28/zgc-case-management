5a856c13ee91de49acbb33c5c91c0ed6d82f4754
Xavier Mukodi <xaviermukodi@gmail.com>
2023-08-16 16:05:21 +0200

    created winstall script

---
+++ ui/electron/winstaller.js
@@ -0,0 +1,24 @@
+const electronInstaller = require('electron-winstaller');
+const fs = require('fs/promises');
+
+
+
+(async () => {
+   try {
+
+      // get name
+      const packageJson = await fs.readFile(`${__dirname}/package.json`, 'utf-8');
+      const { name } = JSON.parse(packageJson);
+
+      // create installer
+      await electronInstaller.createWindowsInstaller({
+         appDirectory: `${__dirname}/build/${name}-win-x64`,
+         outputDirectory: `${__dirname}/dist/`,
+      });
+
+      console.log('Installer created!');
+      
+   } catch (e) {
+   console.log(`No dice: ${e.message}`);
+   }
+})();
