7bdf264e79803c3b94f64242aa8398b375cce08e
Xavier Mukodi <xaviermukodi@gmail.com>
2023-08-20 17:42:39 +0200

    experimental commit

--- ui/compile-ui.sh
+++ ui/compile-ui.sh
@@ -1,7 +1,18 @@
 
+# replace the constants symlink
+# link with actual file
+# (windows is having issues with it)
+BE_CONSTANTS_PATH="src/backend-constants.js"
+mv $BE_CONSTANTS_PATH temp.js
+cp ../api/constants.js $BE_CONSTANTS_PATH
+
 # compile react
 npm run build
 
 # copy build folder to electon www
 rm -rf electron/www
 cp -r build electron/www/
+
+# restoring the symlink
+rm $BE_CONSTANTS_PATH
+mv temp.js $BE_CONSTANTS_PATH
