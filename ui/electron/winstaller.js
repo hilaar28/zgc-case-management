7c3133a058559b04b62ee6bb39e08cc1f6f4e18f
XavierTM <xaviermukodi@gmail.com>
2023-08-20 20:39:24 +0300

    updated winstaller.js to create multiple installers

--- ui/electron/winstaller.js
+++ ui/electron/winstaller.js
@@ -1,24 +1,45 @@
 const electronInstaller = require('electron-winstaller');
 const fs = require('fs/promises');
+const { glob } = require('glob');
 
 
 
 (async () => {
    try {
 
-      // get name
+      console.clear();
+
+      // get name and description
+      // from package.json
       const packageJson = await fs.readFile(`${__dirname}/package.json`, 'utf-8');
-      const { name } = JSON.parse(packageJson);
+      const { name, description, } = JSON.parse(packageJson);
 
-      // create installer
-      await electronInstaller.createWindowsInstaller({
-         appDirectory: `${__dirname}/build/${name}-win-32-x64`,
-         outputDirectory: `${__dirname}/dist/`,
-      });
+      // create installers
+      const appDirBase = `${__dirname}/build`
+      const dirs = await glob(`${appDirBase}/${name}-win32-*`);
+      
+      for (const i in dirs) {
 
-      console.log('Installer created!');
+         const appDirName = dirs[i]
+            .split(/[\\/]{1}/)
+            .pop();
+
+         console.log(`Creating ${appDirName} installer...`);
+
+         await electronInstaller.createWindowsInstaller({
+            appDirectory: `${appDirBase}/${appDirName}`,
+            outputDirectory: `${__dirname}/dist/${appDirName}`,
+            description,
+            noMsi: true,
+         });
+
+         console.log('Done.');
+
+      }
+
+      console.log('Installers created!');
       
    } catch (e) {
-      console.log(`No dice: ${e.message}`);
+      console.log(`Error: ${e.message}`);
    }
 })();
